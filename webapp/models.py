89c049aa7f20d24e38df922da40b6e4f09dc1bc7
Po Tong <potong616@gmail.com>
2015-12-14 23:27:56 -0800

    Initial DB setup, Family and FamilyCommonName tables created

--- webapp/models.py
+++ webapp/models.py
@@ -1,5 +1,21 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
-# Create your models here.
+
+@python_2_unicode_compatible
+class Family(models.Model):
+    scientific_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.scientific_name
+
+
+@python_2_unicode_compatible
+class FamilyCommonName(models.Model):
+    faimly = models.ForeignKey(Family, on_delete=models.CASCADE)
+    common_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.common_name
