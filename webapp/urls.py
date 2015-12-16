ea88d432557973f2388198e2f5db15805bc27273
Po Tong <potong616@gmail.com>
2015-12-16 11:37:46 -0800

    feat: Put style on index page. fix: Include webapp/urls.py file in commit

---
+++ webapp/urls.py
@@ -0,0 +1,37 @@
+from django.conf.urls import url
+
+from . import views
+
+app_name = 'webapp'
+urlpatterns = [
+    # ex: /wildflowers/
+    url(r'^$', views.index, name='wildflowers_index'),
+    # ex: /wildflowers/by_common_name/
+    url(r'^by_common_name/$',
+        views.ByCommonNameView.as_view(),
+        name='wildflowers_by_common_name'),
+    # ex: /wildflowers/by_scientific_name/
+    url(r'^by_scientific_name/$',
+        views.ByScientificNameView.as_view(),
+        name='wildflowers_by_scientific_name'),
+    # ex: /wildflowers/by_family/
+    url(r'^by_family/$',
+        views.ByFamilyView.as_view(),
+        name='wildflowers_by_family'),
+    # ex: /wildflowers/by_color_range/
+    url(r'^by_color_range/$',
+        views.ByColorRangeView.as_view(),
+        name='wildflowers_by_color_range'),
+    # ex: /wildflowers/family/34/
+    url(r'^family/(?P<pk>[0-9]+)/$',
+        views.InFamilyView.as_view(),
+        name='wildflowers_family'),
+    # ex: /wildflowers/color_range/3/
+    url(r'^color_range/(?P<pk>[0-9]+)/$',
+        views.InColorRangeView.as_view(),
+        name='wildflowers_color_range'),
+    # ex: /wildflowers/detail/45/
+    url(r'^detail/(?P<pk>[0-9]+)/$',
+        views.PlantDetailView.as_view(),
+        name='wildflowers_detail'),
+]
